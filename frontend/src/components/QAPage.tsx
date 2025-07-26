68651c69a5bf4b14fcdbcaae2d8c2f4ed9dd76c9
atsushimemet <noap3b69n@gmail.com>
2025-07-26 11:59:59 +0900

    docs: QAページのアイコン名をマイページの実装上のtitle属性と一致するよう修正

--- frontend/src/components/QAPage.tsx
+++ frontend/src/components/QAPage.tsx
@@ -19,24 +19,24 @@ function QAPage() {
           </h2>
           <ul className="space-y-3 text-gray-700">
             <li className="flex items-start">
-              <span className="mr-2">✏️</span>
-              <span><b>編集アイコン（鉛筆マーク）</b>：記録内容を編集できます。押下すると編集フォームが開きます。</span>
+              <span className="mr-2">📝</span>
+              <span><b>投稿を編集</b>：記録内容を編集できます。押下すると編集フォームが開きます。</span>
             </li>
             <li className="flex items-start">
               <span className="mr-2">🗑️</span>
-              <span><b>削除アイコン（ゴミ箱マーク）</b>：記録を削除します。押下後、確認ダイアログが表示されます。</span>
+              <span><b>投稿を削除</b>：記録を削除します。押下後、確認ダイアログが表示されます。</span>
             </li>
             <li className="flex items-start">
               <span className="mr-2">📋</span>
-              <span><b>Google Todoアイコン（クリップボードマーク）</b>：アクション内容をGoogle Todoに追加できます。押下するとGoogle Todoが開き、内容を貼り付けて登録できます。</span>
+              <span><b>Google TODOに追加</b>：アクション内容をGoogle Todoに追加できます。押下するとGoogle Todoが開き、内容を貼り付けて登録できます。</span>
             </li>
             <li className="flex items-start">
               <span className="mr-2">🔗</span>
-              <span><b>シェアアイコン（リンクマーク）</b>：学び・アクション内容をSNSでシェアできます。140文字以内ならX（旧Twitter）、超過時はnoteのネタとして利用できます。</span>
+              <span><b>Xでシェア / noteのネタに</b>：学び・アクション内容をSNSでシェアできます。140文字以内ならX（旧Twitter）、超過時はnoteのネタとして利用できます。</span>
             </li>
             <li className="flex items-start">
               <span className="mr-2">❓</span>
-              <span><b>機能の使い方アイコン（疑問符マーク）</b>：このボタンを押すと、QAページへのリンク付きツールチップが表示されます。</span>
+              <span><b>機能の使い方</b>：このボタンを押すと、QAページへのリンク付きツールチップが表示されます。</span>
             </li>
           </ul>
         </div>
